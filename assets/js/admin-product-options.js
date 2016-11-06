7d95abb71eb3cb3fb914f9e0cf0de3a244fabbc9
Stanislav Khromov <stanislav+github@khromov.se>
2016-11-06 01:42:06 +0100

    Simplify trigger

--- assets/js/admin-product-options.js
+++ assets/js/admin-product-options.js
@@ -3,16 +3,10 @@
     "use strict";
     $(function () {
 
-        $('#options_group_manufacturer').on('input', '#woocommerce-pricefiles_ean_code', function() {
-
-            var $this = $(this);
-            var val = $this.val();
-            var len = val.length;
-
-            $(document).trigger('EAN_check', [ $this, val ]);
-        });
-
-        $('#options_group_manufacturer').on('blur', '#woocommerce-pricefiles_ean_code', function() {
+        /**
+         * EAN Validation
+         */
+        $('#options_group_manufacturer').on('input blur', '#woocommerce-pricefiles_ean_code', function() {
 
             var $this = $(this);
             var val = $this.val();
@@ -20,10 +14,16 @@
             $(document).trigger('EAN_check', [ $this, val ]);
         });
 
+        /**
+         * EAN validation on load
+         */
         $(window).load(function() {
             $(document).trigger('EAN_check', [ $('#woocommerce-pricefiles_ean_code'), $('#woocommerce-pricefiles_ean_code').val() ]);
         });
 
+        /**
+         * EAN trigger
+         */
         $(document).on('EAN_check', function( e, $this, ean ) {
 
             $this.css('background-position-x', ($this.width() - 15)+'px' );
